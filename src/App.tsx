dd16fcd29e90bb462a30ec8a166e3c9486fc9555
Filipe Pereira <filipepereirasanches@gmail.com>
2024-05-17 20:04:49 -0300

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes/routes';
-import Footer from './components/Footer';
-import NavBar from './components/NavBar';
 
 function App() {
   return (
